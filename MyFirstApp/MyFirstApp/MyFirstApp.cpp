a40d3a98c0fa02011fdb9e4b5273552d562d3a6e
Yuzu <Yuzucchi@PersonalComputer>
2022-11-20 19:27:42 -0600

    New update not sure why

---
+++ MyFirstApp/MyFirstApp/MyFirstApp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int favNumber = 0;
+	cout << "Enter your favorite number between 1 and 100: ";
+	cin >> favNumber;
+	cout << "Amazing!!! That's my favorite number too!\nNo really!!, " << favNumber << " is my favorite number!" << endl;
+	return 0;
+}
